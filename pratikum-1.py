d849cb74f79caf288f11a6b1347094de98bc72a8
RevannCrazy <153715577+RevannCrazy@users.noreply.github.com>
2023-12-13 16:23:49 +0800

    upload file 1

---
+++ pratikum-1.py
@@ -0,0 +1,69 @@
+print ('pratikum-1')
+print ('Nama Lengkap: Muh Revan')
+print ('NIM         : 231031062')
+print ('Prodi       : Sistem Informasi')
+print ()
+angkatan = 2023
+print ('angkatan adalah'+str(angkatan))
+
+print()
+a=20
+print('Data adalah',a)
+print('Tipe adata adalah',type(a))
+
+print()
+b=20.0
+print('Data adalah',b)
+print('Tipe adata adalah',type(b))
+
+print()
+kampus='Institut Teknologi BJ Habibie'
+print('Data adalah',kampus)
+print('Tipe adata adalah',type(kampus))
+
+print()
+log= False
+print('Data adalah',log)
+print('Tipe data adalah',type (log))
+
+print()
+c = complex(2,3)
+print('Data adalah',c)
+print('Tipe data adalah',type(c))
+
+p=10
+q=5
+hasil = p+q
+print ('Hasil',p,'+',q,'=',hasil)
+
+p=10
+q=5
+hasil = p-q
+print ('Hasil',p,'-',q,'=',hasil)
+
+p=10
+q=5
+hasil = p*q
+print ('Hasil',p,'*',q,'=',hasil)
+
+p=10
+q=5
+hasil = p/q
+print ('Hasil',p,'/',q,'=',hasil)
+
+p=10
+q=5
+hasil = p%q
+print ('Hasil',p,'%',q,'=',hasil)
+
+p=10
+q=5
+hasil = p//q
+print ('Hasil',p,'//',q,'=',hasil)
+
+p=10
+q=5
+hasil = p**q
+print ('Hasil',p,'**',q,'=',hasil)
+
+print()
